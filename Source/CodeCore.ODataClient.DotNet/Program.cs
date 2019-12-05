4bb4e49fcf5f59f6543a319034730da56171c00e
Thomas Tomiczek <t.tomiczek@nettecture.com>
2019-12-05 22:15:23 +0100

    Nuget updates Add Dotnet code generator empty frame

---
+++ Source/CodeCore.ODataClient.DotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeCore.ODataClient.DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
